f7cb86c7c6446c9045adc0547049e7931a02bf34
abibilonilombardi <31183833+abibilonilombardi@users.noreply.github.com>
2024-02-08 16:18:09 -0300

    Actualizar ejercicio2

---
+++ src/ejercicio2/ejercicio2.spec.ts
@@ -0,0 +1,10 @@
+import {getMovieInfoByName, MovieInfo} from './ejercicio2'
+
+test('#ejercicio2', async () => {
+   const movie: MovieInfo = await getMovieInfoByName('joker')
+   expect(movie.name).toBe('Joker')
+   expect(movie.year).toBe('2019')
+   expect(movie.genre).toBe('Crime, Drama, Thriller')
+   expect(movie.country).toBe('United States, Canada')
+   expect(movie.metacriticRating).toBe('59/100')
+})
