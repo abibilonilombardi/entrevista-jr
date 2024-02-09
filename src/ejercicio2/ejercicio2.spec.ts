4f43b73961097c0729bf16e1160f357594f84f43
abibilonilombardi <31183833+abibilonilombardi@users.noreply.github.com>
2024-02-09 10:06:08 -0300

    Update ejercicio2.spec.ts

--- src/ejercicio2/ejercicio2.spec.ts
+++ src/ejercicio2/ejercicio2.spec.ts
@@ -5,6 +5,6 @@ test('#ejercicio2', async () => {
    expect(movie.name).toBe('Joker')
    expect(movie.year).toBe('2019')
    expect(movie.genre).toBe('Crime, Drama, Thriller')
-   expect(movie.country).toBe('United States, Canada')
+   expect(movie.country).toBe('United States, Canada, Australia')
    expect(movie.metacriticRating).toBe('59/100')
 })
